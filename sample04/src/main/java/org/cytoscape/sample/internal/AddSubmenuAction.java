192f37ae60dfe2bb7a330fbc4fc47c19e97b03e7
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-02-15 23:59:41 +0000

    Original creation

---
+++ sample04/src/main/java/org/cytoscape/sample/internal/AddSubmenuAction.java
@@ -0,0 +1,54 @@
+package org.cytoscape.sample.internal;
+
+import java.awt.event.ActionEvent;
+
+import org.cytoscape.application.swing.AbstractCyAction;
+import org.cytoscape.application.swing.CySwingApplication;
+import org.cytoscape.application.swing.CytoPanel;
+import org.cytoscape.application.swing.CytoPanelName;
+import org.cytoscape.application.swing.CytoPanelState;
+import org.cytoscape.session.CyApplicationManager;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import org.cytoscape.application.swing.CytoPanelComponent;
+
+public class AddSubmenuAction extends AbstractCyAction {
+
+	private CySwingApplication desktopApp;
+
+	public AddSubmenuAction(CyApplicationManager appMgr, CySwingApplication desktopApp){
+		super("sample04...", appMgr);
+		setPreferredMenu("File.Import");
+		//setMenuGravity(2.0f);
+		
+		this.desktopApp = desktopApp;
+	}
+	
+	/**
+	 *  DOCUMENT ME!
+	 *
+	 * @param e DOCUMENT ME!
+	 */
+	public void actionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(this.desktopApp.getJFrame(), "My sub menuitem is clicked", "My message", 
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+	
+	/**
+	 *  DOCUMENT ME!
+	 *
+	 * @return  DOCUMENT ME!
+	 */
+	public boolean isInToolBar() {
+		return false;
+	}
+
+	/**
+	 *  DOCUMENT ME!
+	 *
+	 * @return  DOCUMENT ME!
+	 */
+	public boolean isInMenuBar() {
+		return true;
+	}
+}
