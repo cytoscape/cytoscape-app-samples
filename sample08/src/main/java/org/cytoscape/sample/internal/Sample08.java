e37c41f1c38c9e22e3d40090e6351b067a4a3945
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-02-18 00:27:12 +0000

    Original creation

---
+++ sample08/src/main/java/org/cytoscape/sample/internal/Sample08.java
@@ -0,0 +1,38 @@
+package org.cytoscape.sample.internal;
+
+
+import org.cytoscape.model.events.AddedNodeEvent;
+import org.cytoscape.model.events.AddedNodeListener;
+import org.cytoscape.model.events.NetworkAboutToBeDestroyedEvent;
+import org.cytoscape.model.events.NetworkAboutToBeDestroyedListener;
+import org.cytoscape.model.events.NetworkAddedEvent;
+import org.cytoscape.model.events.NetworkAddedListener;
+import org.cytoscape.view.model.events.NetworkViewAddedEvent;
+import org.cytoscape.view.model.events.NetworkViewAddedListener;
+
+
+public class Sample08 implements NetworkAddedListener, NetworkAboutToBeDestroyedListener,
+				NetworkViewAddedListener, AddedNodeListener {
+
+	public Sample08(){
+	
+	}
+	
+
+	public void handleEvent(NetworkAddedEvent e){
+		System.out.println("Sample08: NetworkAddedEvent is received");
+		System.out.println("\tnetwork name is "+ e.getNetwork().getCyRow().get("name", String.class));
+	}
+
+	public void handleEvent(NetworkAboutToBeDestroyedEvent e){
+		System.out.println("Sample08: NetworkAboutToBeDestroyedEvent is received");
+	}
+	
+	public void handleEvent(NetworkViewAddedEvent e){
+		System.out.println("Sample08: NetworkViewAddedEvent is received!");		
+	}
+
+	public void handleEvent(AddedNodeEvent e){
+		System.out.println("Sample08: AddedNodeEvent is received!");
+	}	
+}
