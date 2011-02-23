8b056d776e66f4d0c9a71eadaeff919f9f9b08af
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-02-23 17:53:52 +0000

    Original creation

---
+++ sample05/src/main/java/org/cytoscape/sample/internal/MyNetworkTask.java
@@ -0,0 +1,53 @@
+package org.cytoscape.sample.internal;
+
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.model.CyNetworkFactory;
+import org.cytoscape.model.CyNetworkManager;
+import org.cytoscape.model.CyNode;
+import org.cytoscape.model.CyTableEntry;
+import org.cytoscape.session.CyApplicationManager;
+import org.cytoscape.session.CyNetworkNaming;
+import org.cytoscape.work.AbstractTask;
+import org.cytoscape.work.TaskMonitor;
+
+public class MyNetworkTask extends AbstractTask {
+	
+	private CyApplicationManager appMgr;
+	private final CyNetworkManager netMgr;
+	private final CyNetworkFactory cnf;
+	private final CyNetworkNaming namingUtil; 
+	
+	public MyNetworkTask(CyApplicationManager appMgr, final CyNetworkManager netMgr, 
+			final CyNetworkNaming namingUtil, final CyNetworkFactory cnf){
+		this.appMgr = appMgr;
+		this.netMgr = netMgr;
+		this.cnf = cnf;
+		this.namingUtil = namingUtil;
+	}
+	
+	public void run(TaskMonitor monitor) {
+		// Create an empty network
+		CyNetwork myNet = cnf.getInstance();
+		myNet.getCyRow().set(CyTableEntry.NAME,
+				      namingUtil.getSuggestedNetworkTitle("My Network"));
+		
+		// Add two nodes to the network
+		CyNode node1 = myNet.addNode();
+		CyNode node2 = myNet.addNode();
+		
+		// Add an edge
+		myNet.addEdge(node1, node2, true);
+				
+		netMgr.addNetwork(myNet);
+		
+		//// The following code will destroy a network
+		boolean destroyNetwork = false;
+		if (destroyNetwork){
+			// Get current network
+			CyNetwork currNet = appMgr.getCurrentNetwork();
+			// Destroy it
+			 netMgr.destroyNetwork(currNet);			
+		}
+	}
+
+}
