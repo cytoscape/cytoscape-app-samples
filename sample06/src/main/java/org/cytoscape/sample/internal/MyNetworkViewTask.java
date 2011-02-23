5809e0810afa8b2e31931ec106d3343e70d39077
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-02-23 18:40:53 +0000

    Original creation

---
+++ sample06/src/main/java/org/cytoscape/sample/internal/MyNetworkViewTask.java
@@ -0,0 +1,52 @@
+package org.cytoscape.sample.internal;
+
+import org.cytoscape.model.CyNetwork;
+import org.cytoscape.session.CyApplicationManager;
+import org.cytoscape.view.model.CyNetworkView;
+import org.cytoscape.view.model.CyNetworkViewFactory;
+import org.cytoscape.view.model.CyNetworkViewManager;
+import org.cytoscape.work.AbstractTask;
+import org.cytoscape.work.TaskMonitor;
+
+public class MyNetworkViewTask extends AbstractTask {
+		
+		private CyApplicationManager appMgr;
+	    private final CyNetworkViewFactory cnvf;
+	    private  final CyNetworkViewManager networkViewManager;
+		
+		public MyNetworkViewTask(CyApplicationManager appMgr, CyNetworkViewFactory cnvf,
+				 final CyNetworkViewManager networkViewManager){
+			this.appMgr = appMgr;
+			this.cnvf = cnvf;
+			this.networkViewManager = networkViewManager;
+		}
+		
+		public void run(TaskMonitor monitor) {
+
+			// Get current network
+			CyNetwork currNet = appMgr.getCurrentNetwork();
+
+			if(currNet == null){
+				return;
+			}
+
+			if(networkViewManager.getNetworkView(currNet.getSUID()) == null){
+				// create a new view for current network
+				CyNetworkView myView = cnvf.getNetworkView(currNet);
+				networkViewManager.addNetworkView(myView);				
+			}
+			else {
+				System.out.println("networkView already existed!");
+			}
+			
+			// The following snippet of code will destroy a networkview
+			boolean destroyView = false;
+			if(destroyView)
+			{
+				CyNetwork currNetwork = appMgr.getCurrentNetwork();
+				CyNetworkView view = networkViewManager.getNetworkView(currNetwork.getSUID());
+				networkViewManager.destroyNetworkView(view);				
+			}
+		}
+
+}
---
+++ sample06/src/main/java/org/cytoscape/sample/internal/Sample06TaskFactory.java
@@ -0,0 +1,25 @@
+package org.cytoscape.sample.internal;
+
+import org.cytoscape.session.CyApplicationManager;
+import org.cytoscape.view.model.CyNetworkViewFactory;
+import org.cytoscape.view.model.CyNetworkViewManager;
+import org.cytoscape.work.TaskFactory;
+import org.cytoscape.work.TaskIterator;
+
+public class Sample06TaskFactory implements TaskFactory {
+
+	private CyApplicationManager appMgr;
+	private CyNetworkViewFactory cnvf;
+	private final CyNetworkViewManager networkViewManager;
+	
+	public Sample06TaskFactory(CyApplicationManager appMgr, CyNetworkViewFactory cnvf,
+			 final CyNetworkViewManager networkViewManager){
+		this.appMgr = appMgr;
+		this.cnvf = cnvf;
+		this.networkViewManager = networkViewManager;
+	}
+	
+	public TaskIterator getTaskIterator(){
+		return new TaskIterator(new MyNetworkViewTask(appMgr, cnvf, networkViewManager));
+	}
+}
