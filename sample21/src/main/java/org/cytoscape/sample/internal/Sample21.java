f019c6fd2d61a26b89c6b074ec030f3107f693db
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-02-25 21:35:07 +0000

    Original creation

---
+++ sample21/src/main/java/org/cytoscape/sample/internal/Sample21.java
@@ -0,0 +1,68 @@
+package org.cytoscape.sample.internal;
+
+import org.cytoscape.session.events.SessionAboutToBeSavedEvent;
+import org.cytoscape.session.events.SessionLoadedEvent;
+import org.cytoscape.session.events.SessionAboutToBeSavedListener;
+import org.cytoscape.session.events.SessionLoadedListener;
+import java.util.ArrayList;
+import java.io.File;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.util.List;
+
+public class Sample21 implements SessionAboutToBeSavedListener, SessionLoadedListener {
+
+
+	// Save plugin state in files
+	public void handleEvent(SessionAboutToBeSavedEvent e){
+	
+		String tmpDir = System.getProperty("java.io.tmpdir");
+		File propFile = new File(tmpDir, "sample21.props");
+		try {
+			BufferedWriter writer = new BufferedWriter(new FileWriter(propFile));
+			writer.write("Line 1");			
+			writer.newLine();
+			writer.write("Line 2");
+
+			writer.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+		
+		ArrayList<File> files = new ArrayList<File>();
+		files.add(propFile);
+		try {
+			e.addPluginFiles("sample21", files);			
+		}
+		catch (Exception ex){
+			ex.printStackTrace();
+		}
+	}
+	
+
+	// restore plugin state from files
+	public void handleEvent(SessionLoadedEvent e){
+
+		try {
+
+			List<File> files = e.getLoadedSession().getPluginFileListMap().get("sample21");
+
+			File propFile = files.get(0);
+			
+			BufferedReader in = new BufferedReader(new FileReader(propFile));
+
+			String firstLine = in.readLine();			
+			String secondLine = in.readLine();
+			
+			System.out.println("\tsample21.props:  " + firstLine);
+			System.out.println("\tsample21.props:  " + secondLine);
+
+			in.close();
+
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}	
+	}
+}
