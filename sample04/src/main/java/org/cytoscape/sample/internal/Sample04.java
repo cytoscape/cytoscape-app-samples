459555881b97ebf9371b3bbc3fef6aa4387821c9
pwang <pwang@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2011-02-28 23:39:09 +0000

    Renamed class

---
+++ sample04/src/main/java/org/cytoscape/sample/internal/Sample04.java
@@ -0,0 +1,31 @@
+package org.cytoscape.sample.internal;
+
+import java.awt.event.ActionEvent;
+import org.cytoscape.application.swing.AbstractCyAction;
+import org.cytoscape.application.swing.CySwingApplication;
+import org.cytoscape.session.CyApplicationManager;
+import javax.swing.JOptionPane;
+
+public class Sample04 extends AbstractCyAction {
+
+	private CySwingApplication desktopApp;
+
+	public Sample04(CyApplicationManager appMgr, CySwingApplication desktopApp){
+		// Add a sub-menu item -- Plugins->Sample04->sample04
+		super("sample04...", appMgr);
+		setPreferredMenu("Plugins.Sample04");
+		//setMenuGravity(2.0f);
+		
+		this.desktopApp = desktopApp;
+	}
+	
+	/**
+	 *  DOCUMENT ME!
+	 *
+	 * @param e DOCUMENT ME!
+	 */
+	public void actionPerformed(ActionEvent e) {
+		JOptionPane.showMessageDialog(this.desktopApp.getJFrame(), "My sub menuitem is clicked", "My message", 
+				JOptionPane.INFORMATION_MESSAGE);
+	}
+}
