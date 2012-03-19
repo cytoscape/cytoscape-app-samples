d9a5433e2f5c6c69c9e2dfff231aa3ccff82de73
jm <jm@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-03-19 20:46:20 +0000

    Fixed compile error due to changes in service-api

--- sample05a/src/main/java/org/cytoscape/sample/internal/Sample05aApp.java
+++ sample05a/src/main/java/org/cytoscape/sample/internal/Sample05aApp.java
@@ -1,7 +1,7 @@
 package org.cytoscape.sample.internal;
 
-import java.util.Dictionary;
 import java.util.Hashtable;
+import java.util.Properties;
 import org.cytoscape.app.AbstractCyApp;
 import org.cytoscape.app.CyAppAdapter;
 import org.cytoscape.work.TaskFactory;
@@ -14,11 +14,11 @@ public class Sample05aApp extends AbstractCyApp {
 	public Sample05aApp(CyAppAdapter a){
 		super(a);
 		
-		Dictionary<String, String> dict = new Hashtable<String, String>();
-		dict.put("title", "Sample5a");
-		dict.put("preferredMenu", "Apps");
-		dict.put("menuGravity", "12.0");
+		Properties properties = new Properties();
+		properties.put("title", "Sample5a");
+		properties.put("preferredMenu", "Apps");
+		properties.put("menuGravity", "12.0");
 
-		a.getCyServiceRegistrar().registerService( new Sample05aTaskFactory(a), TaskFactory.class, dict);
+		a.getCyServiceRegistrar().registerService( new Sample05aTaskFactory(a), TaskFactory.class, properties);
 	}
 }
