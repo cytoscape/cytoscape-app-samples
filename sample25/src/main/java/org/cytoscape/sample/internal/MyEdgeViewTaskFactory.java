d5d25091d1512fe7ff9921c2494947a5da1304df
kono <kono@0ecc0d97-ab19-0410-9704-bfe1a75892f5>
2012-04-13 21:05:15 +0000

    Minor updates only.  No functional changes.

--- sample25/src/main/java/org/cytoscape/sample/internal/MyEdgeViewTaskFactory.java
+++ sample25/src/main/java/org/cytoscape/sample/internal/MyEdgeViewTaskFactory.java
@@ -1,8 +1,6 @@
 package org.cytoscape.sample.internal;
 
-import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.model.CyEdge;
-import org.cytoscape.task.AbstractNetworkViewTaskFactory;
 import org.cytoscape.task.AbstractEdgeViewTaskFactory;
 import org.cytoscape.view.model.CyNetworkView;
 import org.cytoscape.view.model.View;
@@ -10,12 +8,11 @@ import org.cytoscape.work.TaskIterator;
 
 public class MyEdgeViewTaskFactory extends AbstractEdgeViewTaskFactory {
 
-	public MyEdgeViewTaskFactory(){
-	
+	public MyEdgeViewTaskFactory() {
+
 	}
-	
-	
-	@Override
+
+
 	public TaskIterator createTaskIterator(View<CyEdge> edgeView, CyNetworkView networkView) {
 		return new TaskIterator(new MyEdgeViewTask(edgeView, networkView));
 	}
--- sample25/src/main/java/org/cytoscape/sample/internal/MyNodeViewTaskFactory.java
+++ sample25/src/main/java/org/cytoscape/sample/internal/MyNodeViewTaskFactory.java
@@ -1,8 +1,6 @@
 package org.cytoscape.sample.internal;
 
-import org.cytoscape.application.CyApplicationManager;
 import org.cytoscape.model.CyNode;
-import org.cytoscape.task.AbstractNetworkViewTaskFactory;
 import org.cytoscape.task.AbstractNodeViewTaskFactory;
 import org.cytoscape.view.model.CyNetworkView;
 import org.cytoscape.view.model.View;
@@ -10,12 +8,10 @@ import org.cytoscape.work.TaskIterator;
 
 public class MyNodeViewTaskFactory extends AbstractNodeViewTaskFactory {
 
-	
-	public MyNodeViewTaskFactory(){
+	public MyNodeViewTaskFactory() {
 	}
-	
-	
-	@Override
+
+
 	public TaskIterator createTaskIterator(View<CyNode> nodeView, CyNetworkView networkView) {
 		return new TaskIterator(new MyNodeViewTask(nodeView, networkView));
 	}
