0102b82a6ade73c924a6ecd0560abb60296e0a71
rodche <rodche@gmail.com>
2013-05-14 16:41:53 -0400

    fixed a typo bug

--- sample29/src/main/java/org/cytoscape/sample/internal/CyActivator.java
+++ sample29/src/main/java/org/cytoscape/sample/internal/CyActivator.java
@@ -19,13 +19,13 @@ public class CyActivator extends AbstractCyActivator {
 		CyApplicationManager cyApplicationManagerServiceRef = getService(bc,CyApplicationManager.class);
 		VisualMappingManager vmmServiceRef = getService(bc,VisualMappingManager.class);
 
-		Sample29TaskFactory sample05TaskFactory = new Sample29TaskFactory(cyApplicationManagerServiceRef, vmmServiceRef);
+		Sample29TaskFactory sample29TaskFactory = new Sample29TaskFactory(cyApplicationManagerServiceRef, vmmServiceRef);
 				
 		Properties sample29TaskFactoryProps = new Properties();
 		sample29TaskFactoryProps.setProperty("preferredMenu","Apps");
 		sample29TaskFactoryProps.setProperty("menuGravity","18.0");
 		sample29TaskFactoryProps.setProperty("title","Sample 29");
-		registerService(bc,sample05TaskFactory,TaskFactory.class, sample29TaskFactoryProps);
+		registerService(bc,sample29TaskFactory,TaskFactory.class, sample29TaskFactoryProps);
 	}
 }
 
