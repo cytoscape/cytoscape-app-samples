ee749639770ac2c3fae33bc65f1dfa306832e61c
Tim Hull <tmhull@ucsd.edu>
2013-08-01 15:28:36 -0700

    Renamed projects to more descriptive names and updated pom files.

---
+++ sample-add-help/src/main/java/org/cytoscape/sample/internal/CyActivator.java
@@ -0,0 +1,20 @@
+package org.cytoscape.sample.internal;
+
+import java.util.Properties;
+
+import org.cytoscape.application.swing.CyHelpBroker;
+import org.cytoscape.service.util.AbstractCyActivator;
+import org.osgi.framework.BundleContext;
+
+public class CyActivator extends AbstractCyActivator {
+
+	@Override
+	public void start(BundleContext context) throws Exception {
+		
+		CyHelpBroker cyHelpBroker = getService(context, CyHelpBroker.class);
+
+		AppHelp action = new AppHelp(cyHelpBroker);
+		registerAllServices(context, action, new Properties());
+	}
+
+}
