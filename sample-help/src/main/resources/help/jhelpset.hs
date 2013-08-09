17f3f6c47218c8f81819ee8a805ebb877c30a728
Tim Hull <tmhull@ucsd.edu>
2013-08-09 13:18:00 -0700

    Updated to remove cytoscape-thirdparty dependency and references to "Sample 24"

--- sample-help/src/main/resources/help/jhelpset.hs
+++ sample-help/src/main/resources/help/jhelpset.hs
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <helpset version="2.0">
-   <title>PanGIA User Manual</title>
+   <title>Sample Help</title>
    <maps>
       <homeID>top</homeID>
       <mapref location="jhelpmap.jhm"/>
