7e120a3e8a5659ec715b48f28c4cdaaea2195a7a
Samad Lotia <samad.lotia@gladstone.ucsf.edu>
2015-01-15 17:03:14 -0800

    Added project template

---
+++ project-template/src/main/java/edu/ucsf/rbvi/internal/myapp/CyActivator.java
@@ -0,0 +1,40 @@
+package edu.ucsf.rbvi.myapp.internal;
+
+import org.cytoscape.service.util.AbstractCyActivator;
+import org.osgi.framework.BundleContext;
+
+/**
+ * {@code CyActivator} is a class that is a starting point for OSGi bundles.
+ * 
+ * A quick overview of OSGi: The common currency of OSGi is the <i>service</i>.
+ * A service is merely a Java interface, along with objects that implement the
+ * interface. OSGi establishes a system of <i>bundles</i>. Most bundles import
+ * services. Some bundles export services. Some do both. When a bundle exports a
+ * service, it provides an implementation to the service's interface. Bundles
+ * import a service by asking OSGi for an implementation. The implementation is
+ * provided by some other bundle.
+ * 
+ * When OSGi starts your bundle, it will invoke {@CyActivator}'s
+ * {@code start} method. So, the {@code start} method is where
+ * you put in all your code that sets up your app. This is where you import and
+ * export services.
+ * 
+ * Your bundle's {@code Bundle-Activator} manifest entry has a fully-qualified
+ * path to this class. It's not necessary to inherit from
+ * {@code AbstractCyActivator}. However, we provide this class as a convenience
+ * to make it easier to work with OSGi.
+ *
+ * Note: AbstractCyActivator already provides its own {@code stop} method, which
+ * {@code unget}s any services we fetch using getService().
+ */
+public class CyActivator extends AbstractCyActivator {
+	/**
+	 * This is the {@code start} method, which sets up your app. The
+	 * {@code BundleContext} object allows you to communicate with the OSGi
+	 * environment. You use {@code BundleContext} to import services or ask OSGi
+	 * about the status of some service.
+	 */
+	@Override
+	public void start(BundleContext context) throws Exception {
+	}
+}
